f90dc83a7332a2cddabe8ea2c1a60a7c00fb223b
julianjohannesen <github email address>
2018-11-23 18:58:32 -0500

    done

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-
-gulp.task("default", function() {
-	// place code for your default task here
-	browserSync.init({
-		server: "./"
-	});
-});
